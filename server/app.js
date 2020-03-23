b63c426780ebe68b7c8dbaded8a39497c37f06ca
debindv <debinptpm@gmail.com>
2020-03-23 16:57:38 +0530

    Debins First Commit

--- server/app.js
+++ server/app.js
@@ -1,9 +1,41 @@
-var express = require("express"),
-    app = express(),
-    http = require("http").server(app).listen(3000);
+var express = require("express");
+var app = express();
+var http = require('http');
+var upload = require("express-fileupload");
+app.use(upload());
 
-console.log("server started")
+var port = '3000';
+app.set('port',port);
+
+var server = http.createServer(app);
+
+server.listen(port, console.log('Server is running on port 3000'));
+
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
     
-})
+});
+app.post("/",function(req,res){
+    if(req.files){
+        var file = req.files.filename,
+            filename = req.files.filename;
+        file.mv("./upload/"+filename,function(err){
+            if(err){
+                console.log(err);
+                res.send("error occured");
+            }
+            else{
+                console.log("File Uploaded")
+                res.send("Done");
+            }
+        });
+    }
+});
+
+
+
+
+
+
+
+module.exports = app;
